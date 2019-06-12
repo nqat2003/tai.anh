c172ffb2e7d2a924edba2d32001feba636831cc8
Anh Tai <nqat2003@gmail.com>
2019-06-12 16:23:16 +0700

    + Add method to test class

--- OOP/Object_ClassMyPoint/Object_ClassMyPoint/Object_ClassMyPoint.cpp
+++ OOP/Object_ClassMyPoint/Object_ClassMyPoint/Object_ClassMyPoint.cpp
@@ -4,9 +4,18 @@
 #include "stdafx.h"
 #include "MyPoint.h"
 #include "iostream"
-
-int main()
+using namespace std;
+void main()
 {
-    return 0;
+	MyPoint *a = new MyPoint(5, 5);
+	MyPoint *b = new MyPoint(2, 3);
+	cout << "A : ";
+	a->Display();
+	cout << "B : ";
+	b->Display();
+	cout << "Distance between A and B is [" << a->Distance(*b) << "]"<< endl;
+	delete a;
+	delete b;
+	system("pause");
 }
 
