fdc84d04d596f5e934cbd5397e4f6bbcab21924e
Anh Tai <nqat2003@gmail.com>
2019-06-17 10:03:07 +0700

    + Add get set function in PureVirus

--- OOP/virus_revolutionary/virus_revolutionary/PureVirus.h
+++ OOP/virus_revolutionary/virus_revolutionary/PureVirus.h
@@ -17,9 +17,9 @@ public:
 	virtual void DoDie() = 0;
 	virtual list<PureVirus *> DoClone() = 0;
 	virtual void InitResistance() = 0;
-	//void SetDNA(char*);
-	//char* GetDNA();
-	//void SetResistance(int);
+	void SetDNA(char*);
+	char* GetDNA();
+	void SetResistance(int);
 	int GetResistance();
 };
 
