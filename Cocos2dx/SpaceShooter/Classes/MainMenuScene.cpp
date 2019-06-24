a0da920f4992145c4c14031cee82ed2def93bb24
Anh Tai <nqat2003@gmail.com>
2019-06-24 10:01:04 +0700

    + Add MainMenuScene and It's functions - Update .gitignore file

---
+++ Cocos2dx/SpaceShooter/Classes/MainMenuScene.cpp
@@ -0,0 +1,11 @@
+#include "MainMenuScene.h"
+
+cocos2d::Scene * MainMenuScene::createScene()
+{
+	return nullptr;
+}
+
+bool MainMenuScene::Init()
+{
+	return false;
+}
---
+++ Cocos2dx/SpaceShooter/Classes/MainMenuScene.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "cocos2d.h"
+class MainMenuScene : public cocos2d::Scene
+{
+public:
+	cocos2d::Scene* createScene();
+	virtual bool Init();
+};
