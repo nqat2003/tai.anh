b2f87aac1b5557f2c857c51e60eb636bef71aee7
Anh Tai <nqat2003@gmail.com>
2019-06-24 10:45:53 +0700

    + Add class GamePlayScene and It's functions

---
+++ Cocos2dx/SpaceShooter/Classes/GamePlayScene.cpp
@@ -0,0 +1,29 @@
+#include "GamePlayScene.h"
+
+cocos2d::Scene * GamePlayScene::createScene()
+{
+	return nullptr;
+}
+
+bool GamePlayScene::Init()
+{
+	return false;
+}
+
+void GamePlayScene::update(float)
+{
+}
+
+bool GamePlayScene::onTouchBegan(cocos2d::Touch *, cocos2d::Event *)
+{
+	return false;
+}
+
+bool GamePlayScene::onTouchEnded(cocos2d::Touch *, cocos2d::Event *)
+{
+	return false;
+}
+
+void GamePlayScene::onTouchMoved(cocos2d::Touch *, cocos2d::Event *)
+{
+}
---
+++ Cocos2dx/SpaceShooter/Classes/GamePlayScene.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "cocos2d.h"
+#include <iostream>
+#include <vector>
+class GamePlayScene : public cocos2d::Scene
+{
+private:
+	std::vector<Rock*> m_rocks;
+	SpaceShooter* m_spaceShip;
+public:
+	cocos2d::Scene* createScene();
+	virtual bool Init();
+	void update(float);
+	bool onTouchBegan(cocos2d::Touch*, cocos2d::Event*);
+	bool onTouchEnded(cocos2d::Touch*, cocos2d::Event*);
+	void onTouchMoved(cocos2d::Touch*, cocos2d::Event*);
+};
