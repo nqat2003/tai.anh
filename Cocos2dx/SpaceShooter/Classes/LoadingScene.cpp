69d19debfe2a1ea9a7f90f7f15ea3a12b9d34053
Anh Tai <nqat2003@gmail.com>
2019-06-24 09:10:49 +0700

    + Add class LoadingScene and It's functions

---
+++ Cocos2dx/SpaceShooter/Classes/LoadingScene.cpp
@@ -0,0 +1,15 @@
+#include "LoadingScene.h"
+
+cocos2d::Scene * LoadingScene::createScene()
+{
+	return nullptr;
+}
+
+bool LoadingScene::init()
+{
+	return false;
+}
+
+void LoadingScene::update(float)
+{
+}
